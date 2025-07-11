06b6e9b61fcdaf8d0c7d8992f4257689755f20ca
Deep1511 <deeppatel4318@gmail.com>
2025-07-12 00:20:12 +0530

    Lec-1(JS) Javascript Functions and Data-Types

---
+++ JS/hello.js
@@ -0,0 +1,17 @@
+// console.log("Hello form Deep")
+
+function printDeep(){
+    console.log("Hello Deep")
+}
+
+function bringhBrush(numberOfBrush){
+    console.log(`Hanji, laya ${numberOfBrush} brush`);
+}
+
+function addTwo(ekNum,doNum){
+    return ekNum + doNum
+}
+bringhBrush(4);
+printDeep();
+addTwo(1,1);
+console.log(addTwo(1,1));
